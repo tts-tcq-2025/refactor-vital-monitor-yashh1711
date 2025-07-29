10fe24cddcff3921eb22d2d15558d6c5d1911c92
Yash Nagar <nagar.yash107@gmail.com>
2025-07-29 10:47:36 +0530

    refactor duplicate code

--- monitor.cpp
+++ monitor.cpp
@@ -5,33 +5,35 @@
 #include <iostream>
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
-int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > 102 || temperature < 95) {
+void callLoader()
+{
+  for (int i = 0; i < 6; i++)
+  {
+    cout << "\r* " << flush;
+    sleep_for(seconds(1));
+    cout << "\r *" << flush;
+    sleep_for(seconds(1));
+  }
+}
+
+int vitalsOk(float temperature, float pulseRate, float spo2)
+{
+  if (temperature > 102 || temperature < 95)
+  {
     cout << "Temperature is critical!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
+    callLoader();
     return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
+  }
+  else if (pulseRate < 60 || pulseRate > 100)
+  {
     cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
+    callLoader();
     return 0;
-  } else if (spo2 < 90) {
+  }
+  else if (spo2 < 90)
+  {
     cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
+    callLoader();
     return 0;
   }
   return 1;
